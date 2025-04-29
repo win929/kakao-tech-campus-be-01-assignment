78cf7a5e007018259f37abc8471a269d62688d08
Eojin Lee <win929@naver.com>
2025-04-29 20:25:21 +0900

    feat: Lv.1 클래스 없이 기본적인 연산을 수행할 수 있는 계산기 만들기

--- src/com/example/calculator/Calculator.java
+++ src/com/example/calculator/Calculator.java
@@ -1,7 +1,52 @@
 package com.example.calculator;
 
+import java.util.Scanner;
+
 public class Calculator {
     public static void main(String[] args) {
-        System.out.println("Hello, Calculator!");
+        Scanner sc = new Scanner(System.in);
+
+        while (true) {
+            System.out.print("첫 번째 숫자를 입력하세요: ");
+            int num1 = sc.nextInt();
+            System.out.print("두 번째 숫자를 입력하세요: ");
+            int num2 = sc.nextInt();
+
+            sc.nextLine(); // 남아 있는 '\n' 제거
+
+            System.out.print("사칙연산 기호를 입력하세요: ");
+            char operator = sc.nextLine().charAt(0);
+
+            int result;
+            switch (operator) {
+                case '+':
+                    result = num1 + num2;
+                    break;
+                case '-':
+                    result = num1 - num2;
+                    break;
+                case '*':
+                    result = num1 * num2;
+                    break;
+                case '/':
+                    if (num2 != 0) {
+                        result = num1 / num2;
+                    } else {
+                        System.out.println("나눗셈 연산에서 분모에 0이 입력될 수 없습니다.");
+                        continue;
+                    }
+                    break;
+                default:
+                    System.out.println("잘못된 연산자입니다.");
+                    continue;
+            }
+
+            System.out.println("결과: " + result);
+
+            System.out.println("더 계산하시겠습니까? (exit 입력 시 종료)");
+            if (sc.nextLine().equals("exit")) {
+                break;
+            }
+        }
     }
 }
